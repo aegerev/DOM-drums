2c2ac72407d13ffc425db48d5af4cd57acd2f43a
alex <alexandersegerev@gmail.com>
2024-09-23 16:14:13 -0400

    Added sounds. You want to play rough, we'll show you rough.

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[0].addEventListener("click", function() {
+        let drum1 = new Audio('sounds/drum-1.mp3');
+        drum1.play();
+    });
 
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        var audio = new Audio('sounds/crash.mp3');
-        audio.play();
-    })
-}
+    document.querySelectorAll(".drum")[1].addEventListener("click", function() {
+        let kick = new Audio('sounds/kick-bass.mp3');
+        kick.play();
+    });
+
+    document.querySelectorAll(".drum")[2].addEventListener("click", function() {
+        let drum2 = new Audio('sounds/drum-2.mp3');
+        drum2.play();
+    });
+
+    document.querySelectorAll(".drum")[3].addEventListener("click", function() {
+        let drum3 = new Audio('sounds/drum-3.mp3');
+        drum3.play();
+    });
+
+    document.querySelectorAll(".drum")[4].addEventListener("click", function() {
+        let snare = new Audio('sounds/snare.mp3');
+        snare.play();
+    });
+
+    document.querySelectorAll(".drum")[5].addEventListener("click", function() {
+        let crash = new Audio('sounds/crash.mp3');
+        crash.play();
+    });
+
+    document.querySelectorAll(".drum")[6].addEventListener("click", function() {
+        let drum4 = new Audio('sounds/drum-4.mp3');
+        drum4.play();
+    });
+
+    //     function makeSound(key) {
+    //         switch(key) {
+    //             case 'w': 
+    //                 alert("I got clicked!");
+    //                 // 
+    //             break;
+    
+    //                 default: 
+    //                 var audio = new Audio('sounds/crash.mp3');
+    //                 audio.play();
+    //     }
+        
+    // }
+   
